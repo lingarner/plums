1200f7a44750753a443303aa4d8db7a382a6df77
mamillergirl <90581581+mamillergirl@users.noreply.github.com>
2024-02-01 19:38:41 -0700

    create menu for small and large screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,30 @@
+'use client';
+import { useState, useEffect } from "react";
 import Image from "next/image";
+import BottomMenu from "./components/bottomMenu";
+import SideMenu from "./components/sideMenu";
 
 function Home() {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+
+    const checkScreenSize = () => {
+      setIsSmallScreen(window.innerWidth < 500);
+    };
+
+    window.addEventListener("resize", checkScreenSize);
+    
+    checkScreenSize();
+
+    return () => window.removeEventListener("resize", checkScreenSize);
+  }, []);
+
   return (
-    <main>
+    <main className="bg-gray-100">
+      {isSmallScreen? <BottomMenu /> : <SideMenu/>}
     </main>
   );
 }
 
-export default Home();
+export default Home;
