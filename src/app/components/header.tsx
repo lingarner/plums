dc94ee0ba5f065751087a98b18b8a40fa9d3e32a
lingarner <97457456+lingarner@users.noreply.github.com>
2024-02-10 18:17:49 -0700

    head start

---
+++ src/app/components/header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PlumLogo from '../images/plum_logo.png';
+import Image from 'next/image';
+
+export default function Header() {
+
+    return(
+        <header>
+            
+        </header>
+
+    );
+}
+
